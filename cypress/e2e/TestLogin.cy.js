a454a5d0d8f1ae7fc80dabeff1bacf6b04f6cbe0
Cecilia Urrutia <urrutia.cici@gmail.com>
2025-09-04 15:32:48 -0300

    correccion en dos test login, verifica leyenda usuario no confirmado

--- cypress/e2e/TestLogin.cy.js
+++ cypress/e2e/TestLogin.cy.js
@@ -7,19 +7,24 @@ describe('Ingresa al sitio web y clickea el button Login', ()=>{
       cy.log('Test pass, ha ingresado correctamente')
       cy.contains('button', 'Login').click( {force: true})
       cy.url().should('include', '/auth/login')
+      })
+    })
 
+
+    describe("Deberia poder inciiar sesion con un usuario registrado", ()=>{
       it("Deberia poder loguearse con un usuario ya registrado", ()=>{
+        cy.viewport('iphone-7')
+        cy.visit('https://ticketazo.com.ar/auth/login')
         cy.fixture('userLogin').then((user)=>{
           const email = user.email
           const password = user.password
 
           cy.get('input[data-cy="input-email"]').type(email)
           cy.get('input[data-cy="input-password"]').type(password)
-          cy.get('button[type="submit"]').click()
+          cy.contains('button', 'Login').click()
           cy.get('.text-red-500.text-center.mt-2')
               .should('be.visible')
               .and('contain.text', 'Usuario no confirmado')
         })
         })
-      })
-    })
+    })  
