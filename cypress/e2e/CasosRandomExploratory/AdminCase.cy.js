8c3eaeffc581dda376691204b6385440e496c58a
Ambar ramon <ambarJob007@gmail.com>
2025-09-21 00:01:22 -0400

    Treee

---
+++ cypress/e2e/CasosRandomExploratory/AdminCase.cy.js
@@ -0,0 +1,41 @@
+import Links from "../../support/AccesosYMas/Links";
+import ElementsES from "../../support/AccesosYMas/ElementsES";
+
+describe("Pruebas utilizando el usuario admin", () => {
+    beforeEach(() => {
+        Links.Thome();
+    });
+
+    it.only("verfiicacion de las responsabilidades del admin", () => {
+      ElementsES.LogElement();
+      cy.wait(1000);
+     
+      cy.get('.z-0.group').first()
+       .then($btn => {
+    $btn[0].click();  // accede al elemento nativo y lo clickea
+  }); 
+      //.invoke('click');
+      
+     // cy.get('[href="/tickets/list"]', 'Mis entradas').should('be.visible').click();
+      //.should('exist', 'be.visible', 'be.enabled').click();
+      
+    //    cy.get('[data-cy="input-titulo"]').type('el mejor');{ force: true }
+    //     cy.get('[data-cy="datepicker-fecha"]').select("");
+    //     cy.get('[data-cy="select-edad"] > .inline-flex');
+    //     cy.get('[data-cy="select-genero"] > .inline-flex')
+    //     cy.get('[data-cy="input-horario"]')
+    //     cy.get('[data-cy="input-duracion"]')
+    //     cy.get('[data-cy="select-lugar-evento"]')
+    //     cy.get('.flex.col-span-1 > .tap-highlight-transparent')
+    //     cy.get('.rounded-b-large > .z-0')
+
+    });
+
+    it('Links de redireccionamiento para el admin', () => {
+      ElementsES.LogElement();
+    });
+
+    it('Happy patch, el admin debe poder eliminar eventos', () => {
+      ElementsES.LogElement();
+    })
+})
--- cypress/support/AccesosYMas/ElementsES.js
+++ cypress/support/AccesosYMas/ElementsES.js
@@ -7,6 +7,14 @@ class ElementsDifict{
      cy.contains('mm').type(mm);
      cy.contains('aaaa').type(aaaa);  
     }
+
+    LogElement(){
+        cy.get('.justify-end > .text-sm').click();
+        cy.get('[data-cy="input-email"]').type('admin@admin.com');
+        cy.get('[data-cy="input-password"]').type('admin');
+        cy.get('[data-cy="btn-login"]').click();
+    }
+
 }
 
 export default new ElementsDifict();
